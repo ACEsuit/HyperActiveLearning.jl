563cfeaf8a9c1022a47075660b5c03818b26cab8
Documenter.jl <documenter@juliadocs.github.io>
2022-08-12 17:15:17 +0000

    build based on 6447c0a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HyperActiveLearning","category":"page"},{"location":"#HyperActiveLearning","page":"Home","title":"HyperActiveLearning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HyperActiveLearning.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HyperActiveLearning]","category":"page"}]
+}
